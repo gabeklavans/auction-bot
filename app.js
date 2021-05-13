33cbfe4bdccec06ff928e2644700a3148e6afa9e
Gabriel Klavans <gabeklav@bu.edu>
2021-05-12 22:49:39 -0400

    Got env working

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var telegraf_1 = require("telegraf");
-var dotenv = require("dot-env");
-// if (process.env.BOT_TOKEN === undefined) {
-//   throw new TypeError('BOT_TOKEN must be provided!')
-// }
+var dotenv = __importStar(require("dotenv"));
+dotenv.config();
+if (process.env.BOT_TOKEN === undefined) {
+    throw new TypeError('BOT_TOKEN must be provided!');
+}
 var keyboard = telegraf_1.Markup.inlineKeyboard([
-    telegraf_1.Markup.button.url('❤️', 'http://telegraf.js.org'),
-    telegraf_1.Markup.button.callback('Delete', 'delete')
+    telegraf_1.Markup.button.url("❤️", "http://telegraf.js.org"),
+    telegraf_1.Markup.button.callback("Delete", "delete"),
 ]);
-var bot_tok = "1878804532:AAFxT9Y8DyCD3A2eXIDNEM6M85URFByiSRo";
-var bot = new telegraf_1.Telegraf(bot_tok);
-bot.start(function (ctx) { return ctx.reply('Hello'); });
-bot.help(function (ctx) { return ctx.reply('Help message'); });
-bot.on('message', function (ctx) { return ctx.telegram.sendCopy(ctx.message.chat.id, ctx.message, keyboard); });
-bot.action('delete', function (ctx) { return ctx.deleteMessage(); });
+var bot = new telegraf_1.Telegraf(process.env.BOT_TOKEN);
+bot.start(function (ctx) { return ctx.reply("Hello"); });
+bot.help(function (ctx) { return ctx.reply("Help message"); });
+bot.on("message", function (ctx) {
+    return ctx.telegram.sendCopy(ctx.message.chat.id, ctx.message, keyboard);
+});
+bot.action("delete", function (ctx) { return ctx.deleteMessage(); });
 bot.launch();
 // Enable graceful stop
-process.once('SIGINT', function () { return bot.stop('SIGINT'); });
-process.once('SIGTERM', function () { return bot.stop('SIGTERM'); });
+process.once("SIGINT", function () { return bot.stop("SIGINT"); });
+process.once("SIGTERM", function () { return bot.stop("SIGTERM"); });
--- app.ts
+++ app.ts
@@ -1,23 +1,25 @@
-import { Telegraf, Markup } from 'telegraf'
-const dotenv = require("dot-env")
+import { Telegraf, Markup } from "telegraf";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-// if (process.env.BOT_TOKEN === undefined) {
-//   throw new TypeError('BOT_TOKEN must be provided!')
-// }
+if (process.env.BOT_TOKEN === undefined) {
+  throw new TypeError('BOT_TOKEN must be provided!')
+}
 
 const keyboard = Markup.inlineKeyboard([
-  Markup.button.url('❤️', 'http://telegraf.js.org'),
-  Markup.button.callback('Delete', 'delete')
-])
+  Markup.button.url("❤️", "http://telegraf.js.org"),
+  Markup.button.callback("Delete", "delete"),
+]);
 
-const bot_tok = "1878804532:AAFxT9Y8DyCD3A2eXIDNEM6M85URFByiSRo"
-const bot = new Telegraf(bot_tok)
-bot.start((ctx) => ctx.reply('Hello'))
-bot.help((ctx) => ctx.reply('Help message'))
-bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.message.chat.id, ctx.message, keyboard))
-bot.action('delete', (ctx) => ctx.deleteMessage())
-bot.launch()
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply("Hello"));
+bot.help((ctx) => ctx.reply("Help message"));
+bot.on("message", (ctx) =>
+  ctx.telegram.sendCopy(ctx.message.chat.id, ctx.message, keyboard)
+);
+bot.action("delete", (ctx) => ctx.deleteMessage());
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
