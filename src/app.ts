2cc9ae8f8b3f089c20bb38e3d4e6d56c4fa77054
Gabriel Klavans <gabeklav@bu.edu>
2021-05-13 12:14:34 -0400

    Changed up proj structure

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
 import { Telegraf, Markup } from "telegraf";
-import firebase from "firebase"
-import "firebase/analytics"
-import "firebase/database"
+import firebase from "firebase";
+// import "firebase/analytics";
 import * as dotenv from "dotenv";
+const Graceful = require("@ladjs/graceful");
+const Bree = require("bree");
+const Cabin = require("cabin");
 dotenv.config();
 
 if (process.env.BOT_TOKEN === undefined) {
   throw new TypeError("BOT_TOKEN must be provided!");
 }
 
-// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`,
+  databaseURL: `https://${process.env.PROJECT_ID}-default-rtdb.firebaseio.com`,
   projectId: process.env.PROJECT_ID,
   storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
   messagingSenderId: process.env.SENDER_ID,
@@ -22,6 +23,26 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+// database.ref(`users/hewwo`).set({
+//   user: `hewwo`,
+//   pass: `1234`,
+// });
+
+const bree = new Bree({
+  logger: new Cabin(),
+  jobs: [
+    {
+      name: "notify",
+      cron: "*/1 * * * *",
+    },
+  ],
+});
+
+const graceful = new Graceful({ brees: [bree] });
+graceful.listen();
+bree.start();
 
 const keyboard = Markup.inlineKeyboard([
   Markup.button.url("❤️", "http://telegraf.js.org"),
